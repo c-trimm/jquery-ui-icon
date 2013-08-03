324bec44cb13b0a158335e54135fffce8dac0270
Casey Trimm <me@caseytrimm.com>
2013-08-03 17:08:03 -0400

    Cleaned up code, added comments

--- jquery.ui.icon.js
+++ jquery.ui.icon.js
@@ -2,55 +2,89 @@
 // Author: Casey Trimm
 // Created: May 2010
 (function( $ ) {
+	var prefix = "ui-icon-";
+	
+	function doHover(self) {
+		// If we have a hover icon, use it, else just activate the "active state" of the wrapper.
+		if (self.options.hoverIcon.length > 0) {
+			self.element.toggleClass(prefix + self.options.hoverIcon);
+			self.element.toggleClass(prefix + self.options.icon);
+		}
+		else {
+			self.wrapper.toggleClass("ui-state-active");
+		}
+	}
+
 	$.widget("ui.icon", {
 		options : {
 			icon : "link",
-			hoverIcon : ""
+			hoverIcon : "",
+			outerClass : ""
 		},
-
-		wrapper : $("<span style='border: 0;' />"),
 
 		_create : function() {
-			var self, ops, el, prefix, wrapper;
+			var self = this, ops = this.options, el = this.element;
+			
+			// Set options based on options that were passed.
+			this.options.icon = (ops.icon) ? ops.icon : this.options.icon;
+			this.options.hoverIcon = (ops.hoverIcon) ? ops.hoverIcon : this.options.hoverIcon;
+			this.options.outerClass = (ops.outerClass) ? ops.outerClass : this.options.outerClass;
 
-			self = this;
-			ops = this.options;
-			el = this.element;
-			prefix = "ui-icon-";
-
-			if (ops.hoverIcon !== undefined) {
-				this.options.icon = ops.icon;
-			}
-
-			if (ops.hoverIcon !== undefined) {
-				this.options.hoverIcon = ops.hoverIcon;
-			}
-
+			// Add the icon classes
 			el.addClass("ui-icon").addClass(prefix + this.options.icon);
-			el.wrap(this.wrapper);
-
-			this.wrapper.hover(function() {
-				if (this.options.hoverIcon.length) {
-					el.addClass(prefix + self.options.hoverIcon);
-					el.removeClass(prefx + self.options.icon);
-				}
-				else {
-					self.wrapper.toggleClass("ui-state-active");
-				}
-			}, function() {
-				if (this.options.hoverIcon.length) {
-					el.removeClass(prefix + self.options.hoverIcon);
-					el.addClass(prefix + self.options.icon);
-				}
-				else {
-					self.wrapper.toggleClass("ui-state-active");
-				}
+			
+			// Wrap it in an outer span; this is for positioning and to apply the active state
+			el.wrap("<span style='border: 0;' />");
+			this.wrapper = el.parent();
+			this.wrapper.addClass(this.options.outerClass);
+			
+			// Bindings for hovering
+			this.wrapper.bind("mouseover", function() {
+				doHover(self);
 			});
+			this.wrapper.bind("mouseout", function() {
+				doHover(self);
+			});
+			
+			return this;
 		},
 
-		_destroy : function() {
+		destroy : function() {
 			this.wrapper.after(this.element);
 			this.wrapper.remove();
+			this.element.removeClass("ui-icon");
+			this.element.removeClass(prefix + this.options.icon);
+		},
+		
+		// Getter for wrapper element
+		wrapper : function() {
+			return this.wrapper;
+		},
+		
+		_setOption : function(key, value) {
+			switch(key) {
+				// Update the element class
+				case "icon":
+					this.element.removeClass(prefix + this.options.icon);
+					this.element.addClass(prefix + value);
+					break;
+				
+				// Do nothing, this is only used when hovering
+				case "hoverIcon":
+					break;
+				
+				// Update the wrapper class
+				case "outerClass":
+					this.wrapper.removeClass(this.options.outerClass);
+					this.wrapper.addClass(value);
+					break;
+				
+				// There are no other options
+				default:
+					throw Error("Invalid Class!");
+			}
+			
+			this.options[key] = value;
 		}
 	}); 
 })(jQuery);
